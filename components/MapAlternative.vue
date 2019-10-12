2831cf350e7372d7c1bb638d9beb5a043876925e
fexra <fexra@protonmail.com>
2019-10-12 16:37:09 +0200

    alternative map design foundation

---
+++ components/MapAlternative.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <d3-network :net-nodes="nodes" :net-links="[]" :options="options" />
+  </div>
+</template>
+
+<script>
+import D3Network from 'vue-d3-network'
+
+export default {
+  components: {
+    D3Network
+  },
+  data () {
+    return {
+      force: 100,
+      fX: 1,
+      fY: 1,
+      fMb: true,
+      fC: false,
+      nNodes: 500
+    }
+  },
+  computed: {
+    nodes () {
+      return this.makeNodes()
+    },
+    options () {
+      return {
+        force: this.force,
+        forces: {
+          X: this.fX,
+          Y: this.fY,
+          ManyBody: this.fMb,
+          Center: this.fC
+        },
+        nodeSize: 20,
+        nodeLabels: false
+      }
+    }
+  },
+  methods: {
+    makeNodes () {
+      return Array.apply(null, { length: this.nNodes }).map((value, index) => { return { id: index, _color: this.randomColor(), _size: Math.random() * 30 } })
+    },
+    reset () {
+      this.nNodes++
+    },
+    randomColor () {
+      return 'rgb(' + parseInt(Math.random() * 4) + 0 + ',' + parseInt(Math.random() * 10) + 1 + ',' + 30 + ')'
+    }
+  }
+}
+</script>
